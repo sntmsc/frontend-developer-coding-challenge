94d6e0df11d95d5406a2dea5e3493652d4927601
sntmsc <santiago.hamber@gmail.com>
2022-04-26 12:58:15 -0300

    product section components

--- src/components/products-section/ProductsSection.js
+++ src/components/products-section/ProductsSection.js
@@ -26,7 +26,8 @@ const ProductsOptions = () =>{
         <Flex
         my='40px'
         w='100%'
-        justify='flex-start'>
+        justify='flex-start'
+        position='relative'>
             <Flex
             justify='flex-start'>
                 <Text>
@@ -47,6 +48,26 @@ const ProductsOptions = () =>{
                     <Image
                     img='./assets/icons/black-arrow.png'
                     boxSize='8px'/>
+                </Flex>
+                <Flex
+                position='absolute'
+                top='63px'
+                left='100px'
+                w='256px'
+                h='271px'
+                background='white'
+                border='1px solid #DAE4F2'
+                borderRadius='8px'
+                align='flex-start'>
+                    <Flex
+                    w='100%'
+                    h='51px'
+                    justify='flex-start'
+                    p='12px 24px'>
+                        <Text>
+                            All Products
+                        </Text>
+                    </Flex>
                 </Flex>
                 <Divider/>
                 <Text
@@ -72,6 +93,36 @@ const ProductsOptions = () =>{
                 w='165px'
                 h='43px'
                 m='0 12px'>Highest Price</GradientButton>
+            </Flex>
+            <Flex
+            ml='140px'
+            w='250px'
+            h='59px'
+            p='16px'
+            justify='space-between'
+            align='center'
+            border='1px solid #DAE4F2'
+            borderRadius='16px'>
+                    <Flex
+                    boxSize='40px'
+                    background='#E6EDF7'
+                    borderRadius='8px'>
+                        <Image
+                        img='./assets/icons/chevron-default.svg'
+                        transform='rotate(-180deg)'
+                        boxSize='15px'/>
+                    </Flex>
+                    <Text
+                    background='-webkit-linear-gradient(left,#176FEB, #FF80FF)'>
+                        Page 1 of 2</Text>
+                    <Flex
+                    boxSize='40px'
+                    background='#E5F0FF'
+                    borderRadius='8px'>
+                        <Image
+                        img='./assets/icons/chevron-active.svg'
+                        boxSize='15px'/>
+                    </Flex>
             </Flex>
         </Flex>
     )
