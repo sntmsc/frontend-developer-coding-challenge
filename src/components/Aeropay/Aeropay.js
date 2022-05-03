42830f1dd590a7f46a5a0733edbf91fc1c00ea4b
sntmsc <santiago.hamber@gmail.com>
2022-05-03 13:56:00 -0300

    creating aeropay

--- src/components/Aeropay/Aeropay.js
+++ src/components/Aeropay/Aeropay.js
@@ -1,6 +1,7 @@
 import Image from "../Image/Image"
 import Text from "../Text/Text"
 import { Flex } from "../Flex/styled"
+import GradientButton from "../GradientButton/GradientButton"
 const Aeropay = () =>{
     return(
         <Flex
@@ -29,15 +30,54 @@ const Aeropay = () =>{
             </Flex>
             <Flex
             w='100%'
-            p='24px'>
+            p='24px'
+            direction='column'>
                 <Flex
                 h='148px'
                 w='100%'
                 boxShadow='0px 2px 8px rgba(0, 0, 0, 0.05)'
                 background='#252F3D'
                 borderRadius='8px'>
-
                 </Flex>
+                <Flex
+                w='100%'
+                mt='44px'
+                justify='space-around'>
+                    <GradientButton
+                    w='85.33px'
+                    h='35px'
+                     background='#E6F0FF'
+                     textColor='-webkit-linear-gradient(left,#176FEB, #FF80FF)'
+                     borderRadius='12px'>
+                         1000
+                     </GradientButton>
+                    <GradientButton
+                    w='85.33px'
+                    h='35px'
+                    borderRadius='12px'>
+                        5000
+                    </GradientButton>
+                    <GradientButton
+                    w='85.33px'
+                    h='35px'
+                     background='#E6F0FF'
+                     textColor='-webkit-linear-gradient(left,#176FEB, #FF80FF)'
+                     borderRadius='12px'>
+                         7500
+                     </GradientButton>
+                </Flex>
+                <GradientButton
+                    borderRadius='16px'
+                    w='100%'
+                    h='51px'
+                    m='24px 0 0 0'>
+                        <Image
+                        img='./assets/icons/aeropay-3.svg'
+                        boxSize='24px'
+                        m='0 8px'/>
+                        <Text
+                        background='white'>Add Points</Text>
+                </GradientButton>
             </Flex>
         </Flex>
     )
