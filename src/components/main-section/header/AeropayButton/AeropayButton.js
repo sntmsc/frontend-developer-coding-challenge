44f3883bd28c064f04f7748eb47ae9b7cefe120c
sntmsc <santiago.hamber@gmail.com>
2022-05-12 19:42:33 -0300

    adding user data from api

--- src/components/main-section/header/AeropayButton/AeropayButton.js
+++ src/components/main-section/header/AeropayButton/AeropayButton.js
@@ -1,8 +1,26 @@
 import * as S from  "./styled"
 import Image from "./../../../Image/Image"
 import Text from "./../../../Text/Text"
+import { useState, useEffect } from 'react'
+import fetchHeaders from "../../../../../utils/api/fetchHeaders"
 
 const AeropayButton = ({handleClick}) =>{
+    const [userPoints, setUserPoints] = useState('');
+    const headers = fetchHeaders(process.env.TOKEN);
+
+    useEffect( ()=>{
+        fetch(process.env.API_GET_USER, {
+            method: "GET",
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${process.env.TOKEN}`
+            },
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    },[])
 
     return(
         <S.AeropayButtonContainer
@@ -15,7 +33,7 @@ const AeropayButton = ({handleClick}) =>{
             w='60px'
             weight='600'
             background='gradient'
-            userSelect='none'>10.000</Text>
+            userSelect='none'>{userPoints}</Text>
             <Image
             img='./assets/icons/chevron-default.svg'
             boxSize='24px'
