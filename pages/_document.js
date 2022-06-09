b0193107a70cac3ff7c00bc849f69b6249290758
sntmsc <santiago.hamber@gmail.com>
2022-06-09 12:25:37 -0300

    fixing bugs for build

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
+  render () {
+    return (
+      <Html>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;900&display=swap" rel="stylesheet"/>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+
+
+/*import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -12,4 +41,4 @@ export default function Document() {
       </body>
     </Html>
   )
-}
+}*/
