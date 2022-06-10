7b2a99afa85c496fb30f4fe8839b633dd958e728
sntmsc <santiago.hamber@gmail.com>
2022-06-10 11:31:57 -0300

    og data added

--- pages/_app.js
+++ pages/_app.js
@@ -11,12 +11,6 @@ function MyApp({ Component, pageProps }) {
       <meta name="keywords" content="aerolab challenge"/>
       <link rel="shortcut icon" href="./assets/icons/favicon.svg" />
     </Head>
-    <Head>
-      <meta property="og:image" content="https://github.com/Aerolab/frontend-developer-coding-challenge/blob/main/assets/github-cover-aerolab-frontend-developer-coding-challenge.png"/>
-      <meta property="og:title" content="Aerolab Challenge"/>
-      <meta property="og:image:width" content="600"/>
-      <meta property="og:image:height" content="250"/>
-    </Head>
       <ContextToast>
       <Aeropoints>
         <GlobalStyle/>
